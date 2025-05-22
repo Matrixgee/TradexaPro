f16f8b93a78e454273aa30802ff502d9700c7db9
Dhanielgeek <dhanielknightz46@gmail.com>
2025-05-22 09:08:23 +0100

    made all changes needed

--- src/Routes/AdminPrivate.tsx
+++ src/Routes/AdminPrivate.tsx
@@ -1,30 +1,27 @@
-
 import React from "react";
-import {Navigate} from "react-router-dom";
-import {useSelector} from "react-redux";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import RootState from "../Function/Rootstate";
 
 interface AdminPrivateProps {
-    // path: string;
-    element: React.ReactNode;
-    children?: React.ReactNode;
+  // path: string;
+  element: React.ReactNode;
+  children?: React.ReactNode;
 }
 
-const AdminPrivate: React.FC<AdminPrivateProps> = ({element, children}) => {
-    const userToken = useSelector(
-        (state: RootState) => state.mySlice.token
-    );
-    const user = useSelector(
-        (state: RootState) => state.mySlice.tradeUser
-    );
+const AdminPrivate: React.FC<AdminPrivateProps> = ({ element, children }) => {
+  const userToken = useSelector((state: RootState) => state.mySlice.token);
+  console.log(userToken);
 
-    return userToken === "" || !user.isAdmin ? (
-        <Navigate to="/logs" replace />
-    ) : children ? (
-        <>{children}</>
-    ) : (
-        <>{element}</>
-    );
+  const user = useSelector((state: RootState) => state.mySlice.tradeUser);
+
+  return userToken === "" || !user.isAdmin ? (
+    <Navigate to="/auth/login" replace />
+  ) : children ? (
+    <>{children}</>
+  ) : (
+    <>{element}</>
+  );
 };
 
 export default AdminPrivate;
